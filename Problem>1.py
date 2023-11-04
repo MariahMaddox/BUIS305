e85d51f7647981d3b45753b2b1e18f77cd205934
MariahMaddox <maddoxm0919@students.bowiestate.edu>
2023-11-03 21:03:18 -0400

    grithub(lab3)

---
+++ Problem>1.py
@@ -0,0 +1,5 @@
+length=float(input('enter the length of the rectangle:'))
+breadth=float(input('enter the breadth of the rectangle:'))
+
+area= length*breadth
+print(f'area of rectangle: (17*5){area}')
