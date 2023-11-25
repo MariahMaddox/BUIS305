25019e8bf9ed62cb5f581446303d7f38d7ab7369
MariahMaddox <maddoxm0919@students.bowiestate.edu>
2023-11-25 02:48:11 -0500

    lab4

---
+++ lab4.py
@@ -0,0 +1,17 @@
+for i in range(11):
+    print(i)
+for i in range (1,11):
+    print(i)
+for i in range (1,11,2):
+    print(i)
+
+radius:float=float(input('4'))
+area: 3.14 * radius * radius
+print('area of the circle is:', area)
+
+length= float(input('enter the length of rectangle'))
+width= float(input('enter the width of rectangle'))
+area= length * width
+print(f'area of rectangle: {area}')
+
+
